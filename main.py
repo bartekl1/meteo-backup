787be39cfd16e2fae9a56afd3fac673223b3bb62
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2024-02-06 18:43:01 +0100

    Create local database if don't exists

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from termcolor import colored
 import colorama
 import paramiko
+import mysql.connector
 
 import os
 import sys
 import json
 import getpass
 import platform
+import sqlite3
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 colorama.init()
 
-print(" *********************************************")
-print(" *               meteo-backup                *")
-print(" * Database backup tool for my meteo station *")
-print(" * https://github.com/bartekl1/meteo-backup  *")
-print(" *                  v. 1.0                   *")
-print(" *********************************************\n")
+print(" *************************************************")
+print(" *                 meteo-backup                  *")
+print(" *   Database backup tool for my meteo station   *")
+print(" *   https://github.com/bartekl1/meteo-backup    *")
+print(" *                    v. 1.0                     *")
+print(" *************************************************\n")
 
 try:
     with open('configs.json') as file:
@@ -76,9 +84,9 @@ mysqldump = client.exec_command(mysqldump_command)[1].read().decode()
 mysql_temp_file = open("mysql.tmp.sql", "w+")
 mysql_temp_file.write(mysqldump)
 
-print('Running mysqldump', colored('Done!', 'green'))
-
 client.close()
+
+print('Running mysqldump', colored('Done!', 'green'))
 
 print('Converting MySQL to SQLite', colored('\u2026', 'blue'), end='\r')
 
@@ -89,10 +97,65 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 if platform.system() == "Windows":
     os.system(f'wsl --cd "{dir_path}" eval "./mysql2sqlite mysql.tmp.sql | sqlite3 sqlite.tmp.db" > NUL 2>&1')
 else:
-    os.system(f'eval "./mysql2sqlite mysql.tmp.sql | sqlite3 sqlite.tmp.db" > /dev/null 2>&1')
+    os.system('eval "./mysql2sqlite mysql.tmp.sql | sqlite3 sqlite.tmp.db" > /dev/null 2>&1')
 
 mysql_temp_file.close()
 
 os.remove('mysql.tmp.sql')
 
 print('Converting MySQL to SQLite', colored('Done!', 'green'))
+
+print('Checking databases', colored('\u2026', 'blue'), end='\r')
+
+con = sqlite3.connect('sqlite.tmp.db')
+con.row_factory = dict_factory
+cur = con.cursor()
+
+cur.execute("PRAGMA table_info(readings)")
+
+remote_table = cur.fetchall()
+
+local_mysql_host = configs['local_mysql']['host'] \
+    if 'host' in configs['local_mysql'].keys() else 'localhost'
+local_mysql_port = configs['local_mysql']['port'] \
+    if 'port' in configs['local_mysql'].keys() else 3306
+local_mysql_user = configs['local_mysql']['user'] \
+    if 'user' in configs['local_mysql'].keys() else 'root'
+local_mysql_password = configs['local_mysql']['password'] \
+    if 'password' in configs['local_mysql'].keys() else ''
+local_mysql_database = configs['local_mysql']['database'] \
+    if 'database' in configs['local_mysql'].keys() else 'meteo_backup'
+
+db = mysql.connector.connect(
+    host=local_mysql_host,
+    port=local_mysql_port,
+    user=local_mysql_user,
+    password=local_mysql_password
+)
+
+cursor = db.cursor(dictionary=True)
+
+cursor.execute("SHOW DATABASES")
+
+result = cursor.fetchall()
+
+database_exists = local_mysql_database in [row['Database'] for row in result]
+
+if not database_exists:
+    cursor.execute(f'CREATE DATABASE {local_mysql_database}')
+
+cursor.execute(f'USE {local_mysql_database}')
+
+cursor.execute(f'SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = "readings" AND TABLE_SCHEMA = "{local_mysql_database}"')
+
+result = cursor.fetchall()
+
+if len(result) == 0:
+    primary_key = [row for row in remote_table if row['pk'] == 1][0]['name']
+    query = 'CREATE TABLE readings(' \
+            + ', '.join([f'{row["name"]} {row["type"]} {["", "NOT NULL"][row["notnull"]]} {["", "AUTO_INCREMENT"][int(row["name"] == primary_key)]} {["", "DEFAULT " + str(row["dflt_value"])][int(row["dflt_value"] is not None)]}' for row in remote_table]) \
+            + f', PRIMARY KEY ({primary_key}) )'
+
+    cursor.execute(query)
+
+print('Checking databases', colored('Done!', 'green'))
