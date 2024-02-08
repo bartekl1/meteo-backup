8e1ea3f3935a758d4f9b7f0f13f53ce968f3f142
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2024-02-08 19:23:11 +0100

    Import row if doesn't exists

--- main.py
+++ main.py
@@ -16,153 +16,200 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
-colorama.init()
-
-print(" *************************************************")
-print(" *                 meteo-backup                  *")
-print(" *   Database backup tool for my meteo station   *")
-print(" *   https://github.com/bartekl1/meteo-backup    *")
-print(" *                    v. 1.0                     *")
-print(" *************************************************\n")
-
 try:
-    with open('configs.json') as file:
-        configs = json.load(file)
-except Exception:
-    print(colored('Error! Config file don\'t exist or includes errors.', 'red'))
-    sys.exit(1)
+    colorama.init()
 
-try:
-    host = configs['ssh']['host']
-except Exception:
-    print(colored('Error! No SSH host specified.', 'red'))
-    sys.exit(1)
+    print(" *************************************************")
+    print(" *                 meteo-backup                  *")
+    print(" *   Database backup tool for my meteo station   *")
+    print(" *   https://github.com/bartekl1/meteo-backup    *")
+    print(" *          by @bartekl1         v. 1.0          *")
+    print(" *************************************************\n")
 
-try:
-    username = configs['ssh']['user']
-except Exception:
-    print(colored('Error! No SSH user specified.', 'red'))
-    sys.exit(1)
+    try:
+        with open('configs.json') as file:
+            configs = json.load(file)
+    except Exception:
+        print(colored('Error! Config file don\'t exist or includes errors.', 'red'))
+        sys.exit(1)
 
-password = configs['ssh']['password'] if 'password' in configs['ssh'].keys() \
-    else getpass.getpass("Enter password: ")
+    try:
+        host = configs['ssh']['host']
+    except Exception:
+        print(colored('Error! No SSH host specified.', 'red'))
+        sys.exit(1)
 
-# try:
-#     key = paramiko.RSAKey.from_private_key_file(
-#         os.path.expanduser('~/.ssh/id_rsa'),
-#         password=password) \
-#             if 'key' in configs['ssh'] and configs['ssh']['key'] else None
-# except Exception:
-#     print(colored('Error! Invalid SSH private key or password.', 'red'))
-#     sys.exit(1)
+    try:
+        username = configs['ssh']['user']
+    except Exception:
+        print(colored('Error! No SSH user specified.', 'red'))
+        sys.exit(1)
 
-client = paramiko.client.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-try:
-    client.connect(host, username=username, password=password)
-except Exception:
-    print(colored('Error! SSH connection error.', 'red'))
-    sys.exit(1)
+    password = configs['ssh']['password'] if 'password' in configs['ssh'].keys() \
+        else getpass.getpass("Enter password: ")
 
-remote_mysql_host = configs['remote_mysql']['host'] \
-    if 'host' in configs['remote_mysql'].keys() else 'localhost'
-remote_mysql_port = configs['remote_mysql']['port'] \
-    if 'port' in configs['remote_mysql'].keys() else 3306
-remote_mysql_user = configs['remote_mysql']['user'] \
-    if 'user' in configs['remote_mysql'].keys() else 'root'
-remote_mysql_password = configs['remote_mysql']['password'] \
-    if 'password' in configs['remote_mysql'].keys() else ''
-remote_mysql_database = configs['remote_mysql']['database'] \
-    if 'database' in configs['remote_mysql'].keys() else 'meteo'
+    # try:
+    #     key = paramiko.RSAKey.from_private_key_file(
+    #         os.path.expanduser('~/.ssh/id_rsa'),
+    #         password=password) \
+    #             if 'key' in configs['ssh'] and configs['ssh']['key'] else None
+    # except Exception:
+    #     print(colored('Error! Invalid SSH private key or password.', 'red'))
+    #     sys.exit(1)
 
-mysqldump_command = f'mysqldump --host={remote_mysql_host} --port={remote_mysql_port} --user={remote_mysql_user} --password={remote_mysql_password} {remote_mysql_database}'
+    client = paramiko.client.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        client.connect(host, username=username, password=password)
+    except Exception:
+        print(colored('Error! SSH connection error.', 'red'))
+        sys.exit(1)
 
-print('Running mysqldump', colored('\u2026', 'blue'), end='\r')
+    remote_mysql_host = configs['remote_mysql']['host'] \
+        if 'host' in configs['remote_mysql'].keys() else 'localhost'
+    remote_mysql_port = configs['remote_mysql']['port'] \
+        if 'port' in configs['remote_mysql'].keys() else 3306
+    remote_mysql_user = configs['remote_mysql']['user'] \
+        if 'user' in configs['remote_mysql'].keys() else 'root'
+    remote_mysql_password = configs['remote_mysql']['password'] \
+        if 'password' in configs['remote_mysql'].keys() else ''
+    remote_mysql_database = configs['remote_mysql']['database'] \
+        if 'database' in configs['remote_mysql'].keys() else 'meteo'
 
-mysqldump = client.exec_command(mysqldump_command)[1].read().decode()
+    mysqldump_command = f'mysqldump --host={remote_mysql_host} --port={remote_mysql_port} --user={remote_mysql_user} --password={remote_mysql_password} {remote_mysql_database}'
 
-mysql_temp_file = open("mysql.tmp.sql", "w+")
-mysql_temp_file.write(mysqldump)
+    print('Running mysqldump', colored('\u2026', 'blue'), end='\r')
 
-client.close()
+    mysqldump = client.exec_command(mysqldump_command)[1].read().decode()
 
-print('Running mysqldump', colored('Done!', 'green'))
+    mysql_temp_file = open("mysql.tmp.sql", "w+")
+    mysql_temp_file.write(mysqldump)
 
-print('Converting MySQL to SQLite', colored('\u2026', 'blue'), end='\r')
+    client.close()
 
-# mysql2sqlite_path = os.path.abspath('./mysql2sqlite')
+    print('Running mysqldump', colored('Done!', 'green'))
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+    print('Converting MySQL to SQLite', colored('\u2026', 'blue'), end='\r')
 
-if platform.system() == "Windows":
-    os.system(f'wsl --cd "{dir_path}" eval "./mysql2sqlite mysql.tmp.sql | sqlite3 sqlite.tmp.db" > NUL 2>&1')
-else:
-    os.system('eval "./mysql2sqlite mysql.tmp.sql | sqlite3 sqlite.tmp.db" > /dev/null 2>&1')
+    # mysql2sqlite_path = os.path.abspath('./mysql2sqlite')
 
-mysql_temp_file.close()
+    dir_path = os.path.dirname(os.path.realpath(__file__))
 
-os.remove('mysql.tmp.sql')
+    if platform.system() == "Windows":
+        os.system(f'wsl --cd "{dir_path}" eval "./mysql2sqlite mysql.tmp.sql | sqlite3 sqlite.tmp.db" > NUL 2>&1')
+    else:
+        os.system('eval "./mysql2sqlite mysql.tmp.sql | sqlite3 sqlite.tmp.db" > /dev/null 2>&1')
 
-print('Converting MySQL to SQLite', colored('Done!', 'green'))
+    mysql_temp_file.close()
 
-print('Checking databases', colored('\u2026', 'blue'), end='\r')
+    os.remove('mysql.tmp.sql')
 
-con = sqlite3.connect('sqlite.tmp.db')
-con.row_factory = dict_factory
-cur = con.cursor()
+    print('Converting MySQL to SQLite', colored('Done!', 'green'))
 
-cur.execute("PRAGMA table_info(readings)")
+    print('Checking databases', colored('\u2026', 'blue'), end='\r')
 
-remote_table = cur.fetchall()
+    con = sqlite3.connect('sqlite.tmp.db')
+    con.row_factory = dict_factory
+    cur = con.cursor()
 
-local_mysql_host = configs['local_mysql']['host'] \
-    if 'host' in configs['local_mysql'].keys() else 'localhost'
-local_mysql_port = configs['local_mysql']['port'] \
-    if 'port' in configs['local_mysql'].keys() else 3306
-local_mysql_user = configs['local_mysql']['user'] \
-    if 'user' in configs['local_mysql'].keys() else 'root'
-local_mysql_password = configs['local_mysql']['password'] \
-    if 'password' in configs['local_mysql'].keys() else ''
-local_mysql_database = configs['local_mysql']['database'] \
-    if 'database' in configs['local_mysql'].keys() else 'meteo_backup'
+    cur.execute("PRAGMA table_info(readings)")
 
-db = mysql.connector.connect(
-    host=local_mysql_host,
-    port=local_mysql_port,
-    user=local_mysql_user,
-    password=local_mysql_password
-)
+    remote_table = cur.fetchall()
 
-cursor = db.cursor(dictionary=True)
+    local_mysql_host = configs['local_mysql']['host'] \
+        if 'host' in configs['local_mysql'].keys() else 'localhost'
+    local_mysql_port = configs['local_mysql']['port'] \
+        if 'port' in configs['local_mysql'].keys() else 3306
+    local_mysql_user = configs['local_mysql']['user'] \
+        if 'user' in configs['local_mysql'].keys() else 'root'
+    local_mysql_password = configs['local_mysql']['password'] \
+        if 'password' in configs['local_mysql'].keys() else ''
+    local_mysql_database = configs['local_mysql']['database'] \
+        if 'database' in configs['local_mysql'].keys() else 'meteo_backup'
 
-cursor.execute("SHOW DATABASES")
+    db = mysql.connector.connect(
+        host=local_mysql_host,
+        port=local_mysql_port,
+        user=local_mysql_user,
+        password=local_mysql_password
+    )
 
-result = cursor.fetchall()
+    cursor = db.cursor(dictionary=True)
 
-database_exists = local_mysql_database in [row['Database'] for row in result]
+    cursor.execute("SHOW DATABASES")
 
-if not database_exists:
-    cursor.execute(f'CREATE DATABASE {local_mysql_database}')
+    result = cursor.fetchall()
 
-cursor.execute(f'USE {local_mysql_database}')
+    database_exists = local_mysql_database in [row['Database'] for row in result]
 
-cursor.execute(f'SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = "readings" AND TABLE_SCHEMA = "{local_mysql_database}"')
+    if not database_exists:
+        cursor.execute(f'CREATE DATABASE {local_mysql_database}')
 
-result = cursor.fetchall()
+    cursor.execute(f'USE {local_mysql_database}')
 
-if len(result) == 0:
-    primary_key = [row for row in remote_table if row['pk'] == 1][0]['name']
-    query = 'CREATE TABLE readings(' \
-            + ', '.join([f'{row["name"]} {row["type"]} {["", "NOT NULL"][row["notnull"]]} {["", "AUTO_INCREMENT"][int(row["name"] == primary_key)]} {["", "DEFAULT " + str(row["dflt_value"])][int(row["dflt_value"] is not None)]}' for row in remote_table]) \
-            + f', PRIMARY KEY ({primary_key}) )'
+    cursor.execute(f'SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = "readings" AND TABLE_SCHEMA = "{local_mysql_database}"')
 
-    cursor.execute(query)
-else:
-    local_table_columns = [col["COLUMN_NAME"] for col in result]
-    compatible = False not in [col["name"] in local_table_columns for col in remote_table]
+    result = cursor.fetchall()
 
-    if not compatible:
-        print(colored('Warning! Local table is incompatible.', 'yellow'))
+    if len(result) == 0:
+        primary_key = [row for row in remote_table if row['pk'] == 1][0]['name']
+        query = 'CREATE TABLE readings(' \
+                + ', '.join([f'{row["name"]} {row["type"]} {["", "NOT NULL"][row["notnull"]]} {["", "AUTO_INCREMENT"][int(row["name"] == primary_key)]} {["", "DEFAULT " + str(row["dflt_value"])][int(row["dflt_value"] is not None)]}' for row in remote_table]) \
+                + f', PRIMARY KEY ({primary_key}) )'
 
+        cursor.execute(query)
+    else:
+        local_table_columns = [col["COLUMN_NAME"] for col in result]
+        compatible = False not in [col["name"] in local_table_columns for col in remote_table]
 
-print('Checking databases', colored('Done!', 'green'))
+        if not compatible:
+            print(colored('Warning! Local table is incompatible.', 'yellow'))
+
+    print('Checking databases', colored('Done!', 'green'))
+
+    print('Importing', colored('\u2026', 'blue'), end='\r')
+
+    cur.execute("SELECT COUNT() FROM readings")
+
+    rows_number = cur.fetchall()[0]["COUNT()"]
+    rows_processed = 0
+    last_id = 0
+
+    conflicts = []
+
+    while rows_number > rows_processed:
+        cur.execute(f"SELECT * FROM readings WHERE id > {last_id} LIMIT 100")
+        rows = cur.fetchall()
+
+        cursor.execute(f'SELECT * FROM readings WHERE id BETWEEN %s AND %s', (rows[0]['id'], rows[-1]['id']))
+        result = cursor.fetchall()
+        result = {col['id']: col for col in result}
+
+        rows_to_insert = []
+
+        for row in rows:
+            if row['id'] not in result.keys():
+                rows_to_insert.append(row)
+            else:
+                pass
+
+            rows_processed += 1
+            last_id = row['id']
+
+        if len(rows_to_insert) != 0:
+            sql = 'INSERT INTO readings (' + ', '.join(rows[0].keys()) + \
+                ') VALUES (' + ('%s, ' * len(rows[0].keys()))[:-2] + ')'
+
+            cursor.executemany(sql, [tuple(row.values()) for row in rows])
+            db.commit()
+
+        print('Importing', colored(f'{rows_processed}/{rows_number}', 'blue'), end='\r')
+
+    print('Importing', colored('Done!', 'green'), ' ' * 10)
+
+    print(colored('\nDone!', 'blue'))
+
+    sys.exit(0)
+
+except KeyboardInterrupt:
+    sys.exit(0)
